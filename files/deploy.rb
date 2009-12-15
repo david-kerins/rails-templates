f87274371598784a6d6212380f7db07de3d53f67
Michael van Rooijen <meskyan@gmail.com>
2009-12-15 01:23:52 +0100

    Updated Deployment Recipe

--- files/deploy.rb
+++ files/deploy.rb
@@ -1,159 +1,245 @@
-# Author: Michael van Rooijen
-# Description:
-#  This recipe is designed to work for people using Phusion Passenger,
-#  Git as your repository.
+# Guide
+# Configure the essential configurations below and do the following:
+# 
+#   Repository Creation:
+#     cap deploy:repository:create
+#     git add .
+#     git commit -am "initial commit"
+#     git push origin master
+# 
+#   Initial Deployment:
+#     cap deploy:setup
+#     cap deploy
+#     cap deploy:gems:install
+#     cap deploy:db:migrate
+#     cap deploy:passenger:restart
+#     
+#   Then For Every Update Just Do:
+#     git add .
+#     git commit -am "some other commit"
+#     git push origin master
+#     cap deploy
 #
-# Steps:
-# The First Deployment / Setup
-#  - cap deploy:setup
-#  - cap deploy
-#  - cap deploy:migrate_production_database
-# 
-# Then all the other times:
-#  - cap deploy
-# 
-# 
-# The "Restart" task inside the "Deploy" namespace is what gets initialized at the end of the
-# Capistrano process. In order, other tasks will be initialized to make the deployment work.
-# You can create more tasks and add them to the list of tasks in the "Restart" task.
-# Just be sure that you place them in a good (working) order so that it works.
- 
-# SETTINGS
-# Application Domain (example.domain.com)
-# Be sure to fill in the correct domain name for your web application!
-set :application, "domain.com"
- 
-# Set server where the application is going to be uploaded to.
-# Automatically filled in with the application variable.
-role :web,  application
-role :app,  application
-role :db,   application
- 
-# Crontab
-# Change "application" to whatever you want if you wish to use a different identifier
-# to reduce the chance it might conflict with another application.
-set :crontab_id,  application
- 
-# SSH Address
-# Change "application" to whatever you want if you wish to use a dfferent address to connect to.
-# This can either be an IP or a URL that points at the same IP.
-set :ssh_address,  application
- 
-# Set the user
-# :user       => Set the user that will attempt to access the remote repository
-# :deploy_to  => Set the deployment location (destination) on the remote server
-# :use_sudo   => Set to false
-set :user,      "root" 
-set :deploy_to, "/var/rails/domain.com"
-set :use_sudo,  false
- 
-# Git Repository Location
-# :scm        => Specify the source code management tool you want to use (default is git)
-# :repository => Assign the repository location where the application resides
-# :branch     => Select the branch that capistrano should fetch from (default is master)
-set :scm,         "git"
-set :repository,  "ssh://root@domain.com/var/git/domain.git"
-set :branch,      "master"
- 
-# Required: default_run_options[:pty] - This will allow the user to connect to protected repository after
-# logging in when the system prompts for the server's root password
-# default_run_options => (default is true)
+#   For Apache2 Users
+#     cap deploy:apache:create
+#     cap deploy:apache:destroy
+#
+
+
+# Configuration
+
+# This configuration is essential
+set   :application,         "domain.com" or "some.ip"
+set   :crontab_id,          application
+set   :user,                "root" 
+set   :deploy_to,           "/var/rails/#{application}"
+set   :repository_path,     "/var/git/#{application}.git"
+set   :repository,          "ssh://#{user}@#{application}#{repository_path}"
+
+# The following configuration usually doesn't need to be altered
+set   :scm,                 "git"
+set   :branch,              "master"
+set   :use_sudo,            false
+role  :web,                 application
+role  :app,                 application
+role  :db,                  application
 default_run_options[:pty] = true 
- 
- 
- 
+
+
 namespace :deploy do
+  
+  # Tasks that run after the (cap deploy:setup)
+  
+  desc "Sets up the shared path"
+  task :setup_shared_path do
+    puts "\n\n=== Setting up the shared path! ===\n\n"
+    run "mkdir -p #{shared_path}/db #{shared_path}/assets #{shared_path}/system #{shared_path}/config"
+    system "cap deploy:db:update_yaml"
+  end
+  
+  
+  # Tasks that run after every deployment (cap deploy)
   
   desc "Initializes a bunch of tasks in order after the last deployment process."
   task :restart do
-    puts "\n\n\n\n=== Running Custom Processes! ===\n\n\n\n"
+    puts "\n\n=== Running Custom Processes! ===\n\n"
     create_production_log
-    create_symlinks
-    install_gems
-    setup_crontab
+    setup_symlinks
     set_permissions
-    restart_passenger
+    system 'cap deploy:passenger:restart'
   end
  
  
   # Deployment Tasks
   
-  desc "Restarts Passenger"
-  task :restart_passenger do
-    puts "\n\n\n\n=== Restarting Passenger! ===\n\n\n\n"
-    run "touch #{current_path}/tmp/restart.txt"
+  namespace :passenger do
+
+    desc "Restarts Passenger"
+    task :restart do
+      puts "\n\n=== Restarting Passenger! ===\n\n"
+      run "touch #{current_path}/tmp/restart.txt"
+    end
+
   end
   
   desc "Sets permissions for Rails Application"
   task :set_permissions do
-    puts "\n\n\n\n=== Setting Permissions! ===\n\n\n\n"
+    puts "\n\n=== Setting Permissions! ===\n\n"
     run "chown -R www-data:www-data #{deploy_to}"
   end
   
   desc "Creates the production log if it does not exist"
   task :create_production_log do
     unless File.exist?(File.join(shared_path, 'log', 'production.log'))
-      puts "\n\n\n\n=== Creating Production Log! ===\n\n\n\n"
+      puts "\n\n=== Creating Production Log! ===\n\n"
       run "touch #{File.join(shared_path, 'log', 'production.log')}"
     end
   end
   
   desc "Creates symbolic links from shared folder"
-  task :create_symlinks do
-    puts "\n\n\n\n=== Setting up Symbolic Links! ===\n\n\n\n"
+  task :setup_symlinks do
+    puts "\n\n=== Setting up Symbolic Links! ===\n\n"
     run "ln -nfs #{File.join(shared_path, 'config', 'database.yml')}        #{File.join(release_path, 'config', 'database.yml')}"
     run "ln -nfs #{File.join(shared_path, 'db',     'production.sqlite3')}  #{File.join(release_path, 'db',     'production.sqlite3')}"
     run "ln -nfs #{File.join(shared_path, 'assets')}                        #{File.join(release_path, 'public', 'assets')}"
     run "ln -nfs #{File.join(shared_path, 'system')}                        #{File.join(release_path, 'public', 'system')}"
   end
   
-  desc "Update the crontab file for the Whenever Gem."
-  task :setup_crontab, :roles => :db do
-    puts "\n\n\n\n=== Updating the Crontab! ===\n\n\n\n"
-    run "cd #{release_path} && whenever --update-crontab #{crontab_id}"
-  end
- 
-  desc "Installs any 'not-yet-installed' gems on the production server."
-  task :install_gems do
-    puts "\n\n\n\n=== Installing required RubyGems! ===\n\n\n\n"
-    run "cd #{current_path}; rake gems:install RAILS_ENV=production"
-  end
-  
-  
+
   # Manual Tasks
   
-  desc "Syncs the database.yml file from the local machine to the remote machine"
-  task :sync_database_yaml do
-    puts "\n\n\n\n=== Syncing database yaml to the production server! ===\n\n\n\n"
-    system "rsync -vr --exclude='.DS_Store' config/database.yml #{user}@#{ssh_address}:#{shared_path}/config/"
+  namespace :db do
+  
+    desc "Syncs the database.yml file from the local machine to the remote machine"
+    task :update_yaml do
+      puts "\n\n=== Syncing database yaml to the production server! ===\n\n"
+      system "rsync -vr --exclude='.DS_Store' config/database.yml #{user}@#{application}:#{shared_path}/config/"
+    end
+    
+    desc "Create Production Database"
+    task :create do
+      puts "\n\n=== Setting up Production Database! ===\n\n"
+      run "cd #{current_path}; rake db:migrate RAILS_ENV=production"
+      run "chown -R www-data:www-data #{deploy_to}"
+    end
+  
+    desc "Migrate Production Database"
+    task :migrate do
+      puts "\n\n=== Setting up Production Database! ===\n\n"
+      run "cd #{current_path}; rake db:migrate RAILS_ENV=production"
+      run "chown -R www-data:www-data #{deploy_to}"
+    end
+  
   end
   
-  desc "Migrate Production Database"
-  task :migrate_production_database do
-    puts "\n\n\n\n=== Setting up Production Database! ===\n\n\n\n"
-    run "cd #{current_path}; rake db:migrate RAILS_ENV=production"
-    run "chown -R www-data:www-data #{deploy_to}"
+  namespace :gems do
+    
+    desc "Installs any 'not-yet-installed' gems on the production server or a single gem when the gem= is specified."
+    task :install do
+      if ENV['gem']
+        puts "\n\n=== Installing #{ENV['gem']}! ===\n\n"
+        run "gem install #{ENV['gem']}"
+      else
+        puts "\n\n=== Installing required RubyGems! ===\n\n"
+        run "cd #{current_path}; rake gems:install RAILS_ENV=production"
+      end
+    end
+
   end
- 
-  desc "Resets the production environment"
-  task :reset_rails_environment do
-    run "rm -rf #{deploy_to}"
-    system "cap deploy:setup"
+  
+  namespace :repository do
+    
+    desc "Creates the remote Git repository."
+    task :create do
+      puts "\n\n=== Creating remote Git repository! ===\n\n"
+      run "mkdir -p #{repository_path}"
+      run "cd #{repository_path} && git --bare init"
+      system "git remote add origin #{repository[:repository]}"
+      p "#{repository[:repository]} was added to your git repository as origin/master."
+    end
+    
+    desc "Creates the remote Git repository."
+    task :destroy do
+      puts "\n\n=== destroying remote Git repository! ===\n\n"
+      run "rm -rf #{repository_path}"
+      system "git remote rm origin"
+      p "#{repository[:repository]} (origin/master) was removed from your git repository."
+    end
+
+    desc "Resets the remote Git repository."
+    task :reset do
+      puts "\n\n=== Resetting remove Git repository! ===\n\n"
+      system "cap deploy:repository:destroy"
+      system "cap deploy:repository:create"
+    end
+    
   end
   
+  namespace :environment do
+
+    desc "Creates the production environment"
+    task :create do
+      system "cap deploy:setup"
+    end
+
+    desc "Destroys the production environment"
+    task :destroy do
+      run "rm -rf #{deploy_to}"
+    end
   
-  # Initial Setup Tasks
+    desc "Resets the production environment"
+    task :reset do
+      run "rm -rf #{deploy_to}"
+      system "cap deploy:setup"
+    end
   
-  desc "Sets up the shared path"
-  task :setup_shared_path do
-    puts "\n\n\n\n=== Setting up the shared path! ===\n\n\n\n"
-    run "mkdir -p #{shared_path}/db #{shared_path}/assets #{shared_path}/config"
-    sync_database_yaml
+  end
+  
+  namespace :crontab do
+    
+    desc "Update the crontab file for the Whenever Gem."
+    task :update, :roles => :db do
+      puts "\n\n=== Updating the Crontab! ===\n\n"
+      run "cd #{release_path} && whenever --update-crontab #{crontab_id}"
+    end
+    
+  end
+
+  namespace :apache do
+    
+    desc "Adds Apache2 configuraion and enables them."
+    task :create do
+      puts "\n\n=== Adding Apache2 Virtual Host for #{application}! ===\n\n"
+      config = <<-CONFIG
+      <VirtualHost *:80>
+        ServerName #{application}
+        ServerAlias www.#{application}
+        DocumentRoot #{File.join(deploy_to, 'current', 'public')}
+      </VirtualHost>
+      CONFIG
+      
+      file = File.new("tmp/#{application}", "w")
+      file << config
+      file.close 
+      system "rsync -vr tmp/#{application} #{user}@#{application}:/etc/apache2/sites-available/#{application}"
+      File.delete("tmp/#{application}")
+      run "sudo a2ensite #{application}"
+      run "sudo /etc/init.d/apache2 restart"
+    end
+    
+    desc "Adds Apache2 configuraion and enables them."
+    task :destroy do
+      puts "\n\n=== Removing Apache2 Virtual Host for #{application}! ===\n\n"
+      begin run("a2dissite #{application}"); rescue; end
+      begin run("sudo rm -f /etc/apache2/sites-available/#{application}"); rescue; end
+      begin run("sudo /etc/init.d/apache2 restart"); rescue; end
+    end
+
   end
  
 end
  
  
 # Callbacks
- 
+
 after 'deploy:setup', 'deploy:setup_shared_path'
