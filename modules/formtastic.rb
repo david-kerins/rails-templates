7aa0f1f91ba7d2429bbaa50e13fe517b6af6b137
Michael van Rooijen <meskyan@gmail.com>
2009-12-16 16:10:14 +0100

    Fixed a order bug.

--- modules/formtastic.rb
+++ modules/formtastic.rb
@@ -1,13 +1,13 @@
 # Set the Formtastic Gem
 gem "formtastic"
 
-# Install the Gem
-rake "gems:install",
-  :sudo => true
-
 # Install a plugin that works with Formtastic
 plugin 'validation_reflection',
   :git => 'git://github.com/redinger/validation_reflection.git'
+
+# Install the Gem
+rake "gems:install",
+  :sudo => true
 
 # Generate the Formtastic Stylesheets
 generate("formtastic")
